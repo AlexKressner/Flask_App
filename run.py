1a11136c3569dbbafde65624e446815d8e40cb8e
Alex Kressner <alexkressner@Alexs-MacBook-Air.local>
2019-11-16 14:30:04 +0100

    fixed errors

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import app
 from db import db
 
-db.init.app(app)
+db.init_app(app)
 
 #creates the databasae as defined by the URI and all the tables on the fly before the app runs
 @app.before_first_request
